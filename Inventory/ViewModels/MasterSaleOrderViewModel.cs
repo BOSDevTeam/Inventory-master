58109ce1a13be3984e7a600d63b75579003e5ecb
Aung Myo Sann <blueoceanict1@gmail.com>
2022-11-16 12:29:58 +0630

    changed sale order module

--- Inventory/ViewModels/MasterSaleOrderViewModel.cs
+++ Inventory/ViewModels/MasterSaleOrderViewModel.cs
@@ -16,5 +16,6 @@ namespace Inventory.ViewModels
         public string UserName { get; set; }
         public string CustomerName { get; set; }
         public string LocationName { get; set; }
+
     }
 }
