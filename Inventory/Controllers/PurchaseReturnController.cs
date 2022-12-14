dc0980bc06e5bbf9fcb83fd6af093ee4cb13e85c
Nwe Yi Aung <nweyiaung1990@gmail.com>
2022-12-14 10:44:56 +0630

    added new module structure

---
+++ Inventory/Controllers/PurchaseReturnController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class PurchaseReturnController : MyController
+    {
+        public ActionResult PurchaseReturn()
+        {
+            return View();
+        }
+
+        public ActionResult ListPurchaseReturn()
+        {
+            return View();
+        }
+    }
+}
