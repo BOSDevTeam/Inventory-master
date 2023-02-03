958a2874c682967caf6e7195d83b3bd1307eeaba
Aung Myo Sann <blueoceanict1@gmail.com>
2023-02-03 14:30:24 +0630

    added purchase by supplier report

--- Inventory/Controllers/RpPurchaseItemBySupplierController.cs
+++ Inventory/Controllers/RpPurchaseItemBySupplierController.cs
@@ -3,15 +3,97 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Inventory.Common;
+using Inventory.ViewModels;
+
 
 namespace Inventory.Controllers
 {
     public class RpPurchaseItemBySupplierController : MyController
     {
         // GET: RpPurchaseItemBySupplier
+        RpPurchaseItemBySupplierViewModel purchaseViewModel = new RpPurchaseItemBySupplierViewModel();
+        DataConnectorSQL dataConnectorSQL = new DataConnectorSQL();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult PurchaseItemBySupplierReportFilter()
+        {
+            return View();
+        }
+
+        public ActionResult PurchaseItemBySupplierReport(DateTime fromDate, DateTime toDate)
+        {
+            purchaseViewModel.FromDate = fromDate;
+            purchaseViewModel.ToDate = toDate;
+            List<RpPurchaseItemBySupplierViewModel.TranPurchaseModels> lstTranPurchase = GetPurchaseItemBySupplier(fromDate, toDate);
+            List<RpPurchaseItemBySupplierViewModel.SupplierModels> lstSupplier = GetSupplier(fromDate, toDate);
+            purchaseViewModel.lstTranPurchase = lstTranPurchase;
+            purchaseViewModel.lstSupplier = lstSupplier;
+            return View(purchaseViewModel);
+        }
+
+        private List<RpPurchaseItemBySupplierViewModel.SupplierModels> GetSupplier(DateTime fromDate, DateTime toDate)
+        {
+            List<RpPurchaseItemBySupplierViewModel.SupplierModels> list = new List<RpPurchaseItemBySupplierViewModel.SupplierModels>();
+            RpPurchaseItemBySupplierViewModel.SupplierModels item = new RpPurchaseItemBySupplierViewModel.SupplierModels();
+
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptSupplier, (SqlConnection)getConnction());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpPurchaseItemBySupplierViewModel.SupplierModels();
+                item.SupplierID = Convert.ToInt32(reader["SupplierID"]);
+                item.SupplierName = Convert.ToString(reader["SupplierName"]);
+                list.Add(item);
+            }
+            reader.Close();
+            return list;
+        }
+
+        private List<RpPurchaseItemBySupplierViewModel.TranPurchaseModels> GetPurchaseItemBySupplier(DateTime fromDate, DateTime toDate)
+        {
+            List<RpPurchaseItemBySupplierViewModel.TranPurchaseModels> list = new List<RpPurchaseItemBySupplierViewModel.TranPurchaseModels>();
+            RpPurchaseItemBySupplierViewModel.TranPurchaseModels item = new RpPurchaseItemBySupplierViewModel.TranPurchaseModels();
+
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptPurchaseBySupplier, (SqlConnection)getConnction());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpPurchaseItemBySupplierViewModel.TranPurchaseModels();
+                item.ProductID = Convert.ToInt32(reader["ProductID"]);
+                item.SupplierID = Convert.ToInt32(reader["SupplierID"]);
+                item.ProductCode = Convert.ToString(reader["Code"]);
+                item.ProductName = Convert.ToString(reader["ProductName"]);
+                item.Quantity = Convert.ToInt32(reader["Quantity"]);
+                item.PurchasePrice = Convert.ToInt32(reader["PurPrice"]);
+                item.Discount = Convert.ToInt32(reader["Discount"]);
+                item.Amount = Convert.ToInt32(reader["Amount"]);
+                item.UnitID = Convert.ToInt32(reader["UnitID"]);
+                item.UnitKeyword = Convert.ToString(reader["UnitKeyword"]);
+                list.Add(item);
+            }
+            reader.Close();
+            return list;
+        }
+
+        private object getConnction()
+        {
+            object connection;
+            if (Session[AppConstants.SQLConnection] == null) Session[AppConstants.SQLConnection] = dataConnectorSQL.Connect();
+            connection = Session[AppConstants.SQLConnection];
+            return connection;
         }
     }
 }
--- Inventory/ViewModels/RpPurchaseItemBySupplierViewModel.cs
+++ Inventory/ViewModels/RpPurchaseItemBySupplierViewModel.cs
@@ -7,5 +7,42 @@ namespace Inventory.ViewModels
 {
     public class RpPurchaseItemBySupplierViewModel
     {
+        public RpPurchaseItemBySupplierViewModel()
+        {
+            this.FromDate = new DateTime();
+            this.ToDate = new DateTime();
+            this.lstTranPurchase = new List<TranPurchaseModels>();
+            this.lstSupplier = new List<SupplierModels>();
+        }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public List<TranPurchaseModels> lstTranPurchase { get; set; }
+        public List<SupplierModels> lstSupplier { get; set; }
+
+        public class SupplierModels
+        {
+            public int SupplierID { get; set; }
+            public string SupplierName { get; set; }
+        }
+
+        public class TranPurchaseModels
+        {
+            public int ID { get; set; }
+            public int SaleID { get; set; }
+            public int ProductID { get; set; }
+            public int SupplierID { get; set; }
+            public int Quantity { get; set; }
+            public int PurchasePrice { get; set; }
+            public int Amount { get; set; }
+            public bool IsFOC { get; set; }
+            public int? UnitID { get; set; }
+            public int? CurrencyID { get; set; }
+            public int Discount { get; set; }
+            public int DiscountPercent { get; set; }
+            public string ProductName { get; set; }
+            public string ProductCode { get; set; }
+            public string UnitKeyword { get; set; }
+            public string CurrencyKeyword { get; set; }
+        }
     }
 }
