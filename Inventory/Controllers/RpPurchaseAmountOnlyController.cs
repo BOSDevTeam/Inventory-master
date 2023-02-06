b98d52c01f9eed88dc0650b7ef43484255af241c
Nwe Yi Aung <nweyiaung1990@gmail.com>
2023-02-06 12:21:58 +0630

    prepared for purchase amount only report

---
+++ Inventory/Controllers/RpPurchaseAmountOnlyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class RpPurchaseAmountOnlyController : MyController
+    {
+        public ActionResult PurchaseAmountOnlyReportFilter()
+        {
+            return View();
+        }
+    }
+}
