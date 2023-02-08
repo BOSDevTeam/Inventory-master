64f6f6a68536c8389e1eb08084e4bdddd8b61774
Aung Myo Sann <blueoceanict1@gmail.com>
2023-02-08 11:17:45 +0630

    added purchase amount only report

--- Inventory/Controllers/RpPurchaseAmountOnlyController.cs
+++ Inventory/Controllers/RpPurchaseAmountOnlyController.cs
@@ -3,14 +3,126 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inventory.ViewModels;
+using Inventory.Common;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace Inventory.Controllers
 {
     public class RpPurchaseAmountOnlyController : MyController
     {
+        DataConnectorSQL dataConnectorSQL = new DataConnectorSQL();
+        RpPurchaseAmountOnlyViewModel purchaseAmountViewModel = new RpPurchaseAmountOnlyViewModel();
         public ActionResult PurchaseAmountOnlyReportFilter()
         {
             return View();
+        }
+        public ActionResult PurchaseAmountOnlyReport(DateTime fromDate, DateTime toDate)
+        {
+            purchaseAmountViewModel.FromDate = fromDate;
+            purchaseAmountViewModel.ToDate = toDate;
+            List<RpPurchaseAmountOnlyViewModel.PayMethod> lstPayMethod = GetRptPayMethod(fromDate, toDate);
+            purchaseAmountViewModel.lstPayMethod = lstPayMethod;
+            List<RpPurchaseAmountOnlyViewModel.BankPayment> lstBankPayment = GetBankPayment(fromDate, toDate);
+            purchaseAmountViewModel.lstBankPayment = lstBankPayment;
+            List<RpPurchaseAmountOnlyViewModel.MasterPurchaseModels> lstMasterPurchase = GetPurchaseReport(fromDate, toDate);
+            purchaseAmountViewModel.lstMasterPurchase = lstMasterPurchase;
+            return View(purchaseAmountViewModel);
+        }
+
+        private List<RpPurchaseAmountOnlyViewModel.PayMethod> GetRptPayMethod(DateTime fromDate, DateTime toDate)
+        {
+            List<RpPurchaseAmountOnlyViewModel.PayMethod> payMethodList = new List<RpPurchaseAmountOnlyViewModel.PayMethod>();
+            RpPurchaseAmountOnlyViewModel.PayMethod item = new RpPurchaseAmountOnlyViewModel.PayMethod();
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptPayMethod, (SqlConnection) getConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpPurchaseAmountOnlyViewModel.PayMethod();
+                item.PayMethodID = Convert.ToInt32(reader["PayMethodID"]);
+                item.PayMethodName = Convert.ToString(reader["PayMethodName"]);
+                payMethodList.Add(item);
+            }
+
+            reader.Close();
+            return payMethodList;
+        }
+
+        private List<RpPurchaseAmountOnlyViewModel.BankPayment> GetBankPayment(DateTime fromDate, DateTime toDate)
+        {
+            List<RpPurchaseAmountOnlyViewModel.BankPayment> list = new List<RpPurchaseAmountOnlyViewModel.BankPayment>();
+            RpPurchaseAmountOnlyViewModel.BankPayment item = new RpPurchaseAmountOnlyViewModel.BankPayment();
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptBankPaymentByPurchase, (SqlConnection)getConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpPurchaseAmountOnlyViewModel.BankPayment();
+                item.BankPaymentID = Convert.ToInt32(reader["BankPaymentID"]);
+                item.BankPaymentName = Convert.ToString(reader["BankPaymentName"]);
+                list.Add(item);
+            }
+            reader.Close();
+
+            return list;
+        }
+
+        private List<RpPurchaseAmountOnlyViewModel.MasterPurchaseModels> GetPurchaseReport(DateTime fromDate, DateTime toDate)
+        {
+            List<RpPurchaseAmountOnlyViewModel.MasterPurchaseModels> list = new List<RpPurchaseAmountOnlyViewModel.MasterPurchaseModels>();
+            RpPurchaseAmountOnlyViewModel.MasterPurchaseModels item = new RpPurchaseAmountOnlyViewModel.MasterPurchaseModels();
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptPurchaseAmountOnly, (SqlConnection)getConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpPurchaseAmountOnlyViewModel.MasterPurchaseModels();
+                item.PurchaseID = Convert.ToInt32(reader["PurchaseID"]);
+                item.PurchaseDateTime = Convert.ToString(reader["PurDateTime"]);
+                item.UserVoucherNo = Convert.ToString(reader["UserVoucherNo"]);
+                item.PaymentID = Convert.ToInt32(reader["PaymentID"]);
+                item.PaymentName = Convert.ToString(reader["PaymentName"]);
+                item.TaxAmt = Convert.ToInt32(reader["TaxAmt"]);
+                item.ChargesAmt = Convert.ToInt32(reader["ChargesAmt"]);
+                item.VouDisAmount = Convert.ToInt32(reader["VouDisAmount"]);
+                item.VoucherFOC = Convert.ToInt32(reader["VoucherFOC"]);
+                item.PayPercentAmt = Convert.ToInt32(reader["PayPercentAmt"]);
+                item.AdvancedPay = Convert.ToInt32(reader["AdvancedPay"]);
+                item.Subtotal = Convert.ToInt32(reader["Subtotal"]);
+                item.IsVouFOC = Convert.ToBoolean(reader["IsVouFOC"]);
+                if (item.IsVouFOC == true)
+                {
+                    item.Grandtotal = 0;
+                }
+                else
+                {
+                    item.Grandtotal = Convert.ToInt32(reader["Grandtotal"]);
+                }
+
+                item.PayMethodID = Convert.ToInt32(reader["PayMethodID"]);
+                item.BankPaymentID = Convert.ToInt32(reader["BankPaymentID"]);
+                list.Add(item);
+            }
+            reader.Close();
+
+            return list;
+        }
+
+        private object getConnection()
+        {
+            object connection;
+            if (Session[AppConstants.SQLConnection] == null) Session[AppConstants.SQLConnection] = dataConnectorSQL.Connect();
+            connection = Session[AppConstants.SQLConnection];
+            return connection;
         }
     }
 }
--- Inventory/ViewModels/RpPurchaseAmountOnlyViewModel.cs
+++ Inventory/ViewModels/RpPurchaseAmountOnlyViewModel.cs
@@ -7,5 +7,60 @@ namespace Inventory.ViewModels
 {
     public class RpPurchaseAmountOnlyViewModel
     {
+        public RpPurchaseAmountOnlyViewModel()
+        {
+            this.FromDate = new DateTime();
+            this.ToDate = new DateTime();
+            this.lstPayMethod = new List<PayMethod>();
+            this.lstBankPayment = new List<BankPayment>();
+            this.lstMasterPurchase = new List<MasterPurchaseModels>();
+        }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+
+        public List<PayMethod> lstPayMethod { get; set; }
+
+        public List<BankPayment> lstBankPayment { get; set; }
+        public List<MasterPurchaseModels> lstMasterPurchase { get; set; }
+
+        public class PayMethod
+        {
+            public int PayMethodID { get; set; }
+            public string PayMethodName { get; set; }
+        }
+
+        public class BankPayment
+        {
+            public int BankPaymentID { get; set; }
+            public string BankPaymentName { get; set; }
+
+        }
+
+        public class MasterPurchaseModels
+        {
+            public int PurchaseID { get; set; }
+            public string UserVoucherNo { get; set; }
+            public string PurchaseDateTime { get; set; }
+            public int PaymentID { get; set; }
+            public string PaymentName { get; set; }
+            public int VoucherDiscount { get; set; }
+            public int AdvancedPay { get; set; }
+            public int Tax { get; set; }
+            public int TaxAmt { get; set; }
+            public int Charges { get; set; }
+            public int ChargesAmt { get; set; }
+            public int Subtotal { get; set; }
+            public int Total { get; set; }
+            public int Grandtotal { get; set; }
+            public int VouDisPercent { get; set; }
+            public int VouDisAmount { get; set; }
+            public int PayMethodID { get; set; }
+            public int BankPaymentID { get; set; }
+            public int PaymentPercent { get; set; }
+            public int PayPercentAmt { get; set; }
+            public int VoucherFOC { get; set; }
+            public bool IsVouFOC { get; set; }
+
+        }
     }
 }
