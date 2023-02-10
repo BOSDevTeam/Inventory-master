ff05a1832dd1301efec8ebe47d32f2c66a1a26d9
Aung Myo Sann <blueoceanict1@gmail.com>
2023-02-10 13:51:30 +0630

    added transfer report

--- Inventory/Controllers/RpTransferVoucherController.cs
+++ Inventory/Controllers/RpTransferVoucherController.cs
@@ -3,14 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inventory.ViewModels;
+using Inventory.Common;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Inventory.Controllers
 {
     public class RpTransferVoucherController : MyController
     {
+        DataConnectorSQL dataConnectorSQL = new DataConnectorSQL();
+        RpTransferVoucherViewModel transferVoucherViewModel = new RpTransferVoucherViewModel();
         public ActionResult TransferVoucherReportFilter()
         {
             return View();
+        }
+        public ActionResult TransferVoucherReport(DateTime fromDate, DateTime toDate)
+        {
+            transferVoucherViewModel.FromDate = fromDate;
+            transferVoucherViewModel.ToDate = toDate;
+            List<RpTransferVoucherViewModel.MasterTransferModels> lstMasterTransfer = GetRptMasterTransfer(fromDate, toDate);
+            transferVoucherViewModel.lstMasterTransfer = lstMasterTransfer;
+            List<RpTransferVoucherViewModel.TranTransferModels> lstTranTransfer = GetRptTranTransfer(fromDate, toDate);
+            transferVoucherViewModel.lstTranTransfer = lstTranTransfer;
+            return View(transferVoucherViewModel);
+        }
+
+        public List<RpTransferVoucherViewModel.MasterTransferModels> GetRptMasterTransfer(DateTime fromDate, DateTime toDate)
+        {
+            List<RpTransferVoucherViewModel.MasterTransferModels> list = new List<RpTransferVoucherViewModel.MasterTransferModels>();
+            RpTransferVoucherViewModel.MasterTransferModels item = new RpTransferVoucherViewModel.MasterTransferModels();
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptMasterTransferList, (SqlConnection) getConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpTransferVoucherViewModel.MasterTransferModels();
+                item.TransferID = Convert.ToInt32(reader["TransferID"]);
+                item.TransferDateTime = Convert.ToString(reader["TransferDateTime"]);
+                item.UserVoucherNo = Convert.ToString(reader["UserVoucherNo"]);
+                item.VoucherID = Convert.ToString(reader["VoucherID"]);
+                item.UserName = Convert.ToString(reader["UserName"]);
+                item.FromLocationID = Convert.ToInt32(reader["FromLocationID"]);
+                item.FromLocationName = Convert.ToString(reader["FromLocation"]);
+                item.ToLocationID = Convert.ToInt32(reader["ToLocationID"]);
+                item.ToLocationName = Convert.ToString(reader["ToLocation"]);
+                item.Remark = Convert.ToString(reader["Remark"]);
+                list.Add(item);
+            }
+
+            reader.Close();
+
+            return list;
+        }
+
+        public List<RpTransferVoucherViewModel.TranTransferModels> GetRptTranTransfer(DateTime fromDate, DateTime toDate)
+        {
+            List<RpTransferVoucherViewModel.TranTransferModels> list = new List<RpTransferVoucherViewModel.TranTransferModels>();
+            RpTransferVoucherViewModel.TranTransferModels item = new RpTransferVoucherViewModel.TranTransferModels();
+            SqlCommand cmd = new SqlCommand(Procedure.PrcGetRptTranTransferList, (SqlConnection)getConnection());
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@fromDate", fromDate);
+            cmd.Parameters.AddWithValue("@toDate", toDate);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                item = new RpTransferVoucherViewModel.TranTransferModels();
+                item.TransferID = Convert.ToInt32(reader["TransferID"]);
+                item.ProductID = Convert.ToInt32(reader["ProductID"]);
+                item.ProductName = Convert.ToString(reader["ProductName"]);
+                item.Quantity = Convert.ToInt32(reader["Quantity"]);
+                item.UnitID = Convert.ToInt32(reader["UnitID"]);
+                item.UnitKeyword = Convert.ToString(reader["Keyword"]);
+                list.Add(item);
+            }
+
+            reader.Close();
+
+            return list;
+        }
+
+
+        public object getConnection()
+        {
+            object connection;
+            if (Session[AppConstants.SQLConnection] == null) Session[AppConstants.SQLConnection] = dataConnectorSQL.Connect();
+            connection = Session[AppConstants.SQLConnection];
+            return connection;
         }
     }
 }
--- Inventory/ViewModels/RpTransferItemViewModel.cs
+++ Inventory/ViewModels/RpTransferItemViewModel.cs
@@ -7,5 +7,40 @@ namespace Inventory.ViewModels
 {
     public class RpTransferItemViewModel
     {
+        public RpTransferItemViewModel()
+        {
+            this.FromDate = new DateTime();
+            this.ToDate = new DateTime();
+            this.lstMasterTransfer = new List<MasterTransferModels>();
+            this.lstTranTransfer = new List<TranTransferModels>();
+        }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public List<MasterTransferModels> lstMasterTransfer { get; set; }
+        public List<TranTransferModels> lstTranTransfer { get; set; }
+        public class MasterTransferModels
+        {
+            public int TransferID { get; set; }
+            public int FromLocationID { get; set; }
+            public string FromLocationName { get; set; }
+            public int ToLocationID { get; set; }
+            public string ToLocationName { get; set; }
+        }
+
+        public class TranTransferModels
+        {
+            public int ID { get; set; }
+            public int TransferID { get; set; }
+            public int ProductID { get; set; }
+            public string ProductName { get; set; }
+            public string ProductCode { get; set; }
+            public int PurPrice { get; set; }
+            public int Quantity { get; set; }
+            public int FromLocationID { get; set; }
+            public int ToLocationID { get; set; }
+            public int? UnitID { get; set; }
+            public string UnitKeyword { get; set; }
+
+        }
     }
 }
